29bc87eff85ac46c6bd898cb8aae38e584f20e25
Dellirium <dellirium@dellirium.ru>
2018-11-27 09:26:35 +0300

    homework

--- src/main/java/ru/dellirium/gb/App.java
+++ src/main/java/ru/dellirium/gb/App.java
@@ -1,9 +1,78 @@
 package ru.dellirium.gb;
 
-public class App
-{
+import java.sql.*;
+import java.util.Scanner;
+
+public class App {
+
+    static Scanner scanner = new Scanner(System.in);
+    private static Connection connection;
+    private static Statement statement;
+
     public static void main( String[] args )
     {
+        try {
+            connect();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
 
+        try {
+            statement.execute("CREATE TABLE IF NOT EXISTS Goods (" +
+                    "id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
+                    "prodid INTEGER NOT NULL, " +
+                    "title TEXT, " +
+                    "cost INTEGER)");
+            statement.execute("DELETE FROM Goods");
+
+            connection.setAutoCommit(false);
+            for (int i = 0; i < 10000; i++) {
+                statement.executeUpdate("INSERT INTO Goods(prodid, title, cost)" +
+                        "VALUES (" + i + ", 'товар" + i + "', " + i + "00)");
+            }
+            connection.setAutoCommit(true);
+
+            while (true) {
+                System.out.println("Команда: ");
+                String command = scanner.nextLine();
+                String[] pieces = command.split("\\s+");
+                if (pieces[0].equals("цена")) {
+
+                    ResultSet rs = statement.executeQuery("SELECT cost FROM Goods WHERE title = '" + pieces[1] + "'");
+                    while (rs.next()) {
+                        System.out.println(rs.getInt(1));
+                    }
+
+                } else if (pieces[0].equals("сменитьцену")) {
+                    statement.execute("UPDATE Goods SET cost = " + pieces[2] + " WHERE title = '" + pieces[1] + "'");
+                    System.out.println("Цена успешно изменена");
+                } else if (pieces[0].equals("товарыпоцене")) {
+                    ResultSet rs = statement.executeQuery("SELECT * FROM Goods WHERE cost > " + pieces[1] + " AND cost < " + pieces[2] + "");
+                    while (rs.next()) {
+                        int id = rs.getInt("prodid");
+                        String title = rs.getString("title");
+                        int cost = rs.getInt("cost");
+                        System.out.println(
+                                "id " + id + ", name " + title + ", cost " + cost
+                        );
+                    }
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+/*        try {
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    static void connect() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:main.db");
+        statement = connection.createStatement();
     }
 }
